dbdc0f829d96baed703d64dd6e136549fac6c1fe
coinagesoft <prasad.a1405@gmail.com>
2026-05-07 13:39:02 +0530

    chnages

--- src/app/technology-services/page.js
+++ src/app/technology-services/page.js
@@ -4,7 +4,6 @@ import "./technology-services.css";
 export default function TechnologyServices() {
   return (
     <div className="ts-page">
-
       {/* HERO */}
       <section className="ts-intro-section">
         <div className="ts-intro-box">
@@ -16,19 +15,22 @@ export default function TechnologyServices() {
       <section className="ts-overview">
         <h2>Overview</h2>
         <p>
-          Technology is advancing in all dimensions, and our expert technology teams help our
-          clients solve business problems with innovative solutions. Our emphasis is on addressing
-          business challenges and driving innovation and digitization with the latest technological
-          advancements. We find the best approach for every business problem by leveraging
-          state-of-the-art technologies. We have solved critical business issues using Generative
-          AI models and internally developed AI/ML models.
+          Technology is advancing in all dimensions, and our expert technology
+          teams help our clients solve business problems with innovative
+          solutions. Our emphasis is on addressing business challenges and
+          driving innovation and digitization with the latest technological
+          advancements. We find the best approach for every business problem by
+          leveraging state-of-the-art technologies. We have solved critical
+          business issues using Generative AI models and internally developed
+          AI/ML models.
         </p>
         <p>
-          In summary, our primary focus is to provide your organization with forward-looking
-          technology consulting services. Whether you are looking to bind technological
-          advancements, integrate new systems after an acquisition, or optimize your existing
-          tech infrastructure, our technology consulting services are here to pave the way
-          for your success.
+          In summary, our primary focus is to provide your organization with
+          forward-looking technology consulting services. Whether you are
+          looking to bind technological advancements, integrate new systems
+          after an acquisition, or optimize your existing tech infrastructure,
+          our technology consulting services are here to pave the way for your
+          success.
         </p>
       </section>
 
@@ -36,13 +38,18 @@ export default function TechnologyServices() {
       <section className="ts-services">
         <h2>Our Technology Services Include</h2>
         <div className="ts-cards-grid">
-
           <div className="ts-card">
             <h3>Transformation</h3>
             <ul>
-              <li>Migration of interfaces from legacy technologies to modern integration architecture</li>
+              <li>
+                Migration of interfaces from legacy technologies to modern
+                integration architecture
+              </li>
               <li>Migration from SAP PO/PI to SAP CPI</li>
-              <li>Interface Development - SAP CPI, Boomi, Talend, Azure Integration Services, etc.</li>
+              <li>
+                Interface Development - SAP CPI, Boomi, Talend, Azure
+                Integration Services, etc.
+              </li>
             </ul>
           </div>
 
@@ -58,16 +65,25 @@ export default function TechnologyServices() {
           <div className="ts-card">
             <h3>Legacy Application Modernization</h3>
             <ul>
-              <li>Assess and develop a roadmap for legacy application modernization</li>
+              <li>
+                Assess and develop a roadmap for legacy application
+                modernization
+              </li>
               <li>Migrate applications</li>
-              <li>Develop applications using low/no code platforms like MS Power Platform</li>
+              <li>
+                Develop applications using low/no code platforms like MS Power
+                Platform
+              </li>
             </ul>
           </div>
 
           <div className="ts-card">
             <h3>Integration Services</h3>
             <ul>
-              <li>Define next generation integration architecture and set up integration CoE to support digital transformation</li>
+              <li>
+                Define next generation integration architecture and set up
+                integration CoE to support digital transformation
+              </li>
             </ul>
           </div>
 
@@ -75,154 +91,150 @@ export default function TechnologyServices() {
             <h3>Application Development (SAP and Non-SAP)</h3>
             <ul>
               <li>SAP S/4 HANA and ECC - on-premises, cloud and extension</li>
-              <li>SAP S/4HANA (On-Premise and Cloud) - custom development and extensions</li>
+              <li>
+                SAP S/4HANA (On-Premise and Cloud) - custom development and
+                extensions
+              </li>
             </ul>
           </div>
 
           <div className="ts-card">
             <h3>Application Security Services (SAP and Non-SAP)</h3>
             <ul>
-              <li>Define application security architecture to support digital transformation</li>
+              <li>
+                Define application security architecture to support digital
+                transformation
+              </li>
               <li>Application security Services</li>
               <li>SAP security services</li>
             </ul>
           </div>
-
         </div>
       </section>
 
       {/* OUR EXPERTISE */}
       <section className="ts-expertise">
         <h2>Our Expertise</h2>
+
         <p className="ts-expertise-intro">
-          In the wake of events like mergers and acquisitions, internal revamps, market
-          disruptions, and technology upgrades, the demand for technology consulting services
-          becomes vital. This is where Taciti&apos;s expertise takes center stage, as we specialize
-          in providing a host of tailored technology consulting solutions to address your
-          organization&apos;s specific needs and objectives.
+          In the wake of events like mergers and acquisitions, internal revamps,
+          market disruptions, and technology upgrades, the demand for technology
+          consulting services becomes vital. This is where Taciti&apos;s
+          expertise takes center stage, as we specialize in providing a host of
+          tailored technology consulting solutions to address your
+          organization&apos;s specific needs and objectives. These solutions are
+          a fusion of cutting-edge technologies, industry best practices and
+          innovative management techniques.
         </p>
+
         <div className="ts-expertise-grid">
           <div className="ts-expertise-item">
             <h3>Navigating Tech-Driven Change</h3>
-            <ul>
-              <li>
-                <svg
-  className="ts-arrow-svg"
-  aria-hidden="true"
-  viewBox="0 0 512 512"
-  xmlns="http://www.w3.org/2000/svg"
->
-  <path d="M504 256C504 119 393 8 256 8S8 119 8 256s111 248 248 248 248-111 248-248zm-448 0c0-110.5 89.5-200 200-200s200 89.5 200 200-89.5 200-200 200S56 366.5 56 256zm72 20v-40c0-6.6 5.4-12 12-12h116v-67c0-10.7 12.9-16 20.5-8.5l99 99c4.7 4.7 4.7 12.3 0 17l-99 99c-7.6 7.6-20.5 2.2-20.5-8.5v-67H140c-6.6 0-12-5.4-12-12z" />
-</svg>
-                Guide organizations through mergers, acquisitions, and critical technology upgrades
-              </li>
-              <li>
-                <svg
-  className="ts-arrow-svg"
-  aria-hidden="true"
-  viewBox="0 0 512 512"
-  xmlns="http://www.w3.org/2000/svg"
->
-  <path d="M504 256C504 119 393 8 256 8S8 119 8 256s111 248 248 248 248-111 248-248zm-448 0c0-110.5 89.5-200 200-200s200 89.5 200 200-89.5 200-200 200S56 366.5 56 256zm72 20v-40c0-6.6 5.4-12 12-12h116v-67c0-10.7 12.9-16 20.5-8.5l99 99c4.7 4.7 4.7 12.3 0 17l-99 99c-7.6 7.6-20.5 2.2-20.5-8.5v-67H140c-6.6 0-12-5.4-12-12z" />
-</svg>
-                Expert technology consulting for seamless transitions
-              </li>
-            </ul>
+
+            <p>
+              In today&apos;s fast-paced business landscape, technology is often
+              at the forefront of change. Whether your organization is
+              struggling with the complexities of a merger or acquisition, or
+              implementing critical technology upgrades, we are uniquely
+              positioned to guide you through these transitions with our
+              technology consulting expertise.
+            </p>
           </div>
+
           <div className="ts-expertise-item">
             <h3>Leveraging Technological Advancements</h3>
-            <ul>
-              <li>
-                <svg
-  className="ts-arrow-svg"
-  aria-hidden="true"
-  viewBox="0 0 512 512"
-  xmlns="http://www.w3.org/2000/svg"
->
-  <path d="M504 256C504 119 393 8 256 8S8 119 8 256s111 248 248 248 248-111 248-248zm-448 0c0-110.5 89.5-200 200-200s200 89.5 200 200-89.5 200-200 200S56 366.5 56 256zm72 20v-40c0-6.6 5.4-12 12-12h116v-67c0-10.7 12.9-16 20.5-8.5l99 99c4.7 4.7 4.7 12.3 0 17l-99 99c-7.6 7.6-20.5 2.2-20.5-8.5v-67H140c-6.6 0-12-5.4-12-12z" />
-</svg>
-                Harness the latest advancements to enhance operational efficiency
-              </li>
-              <li>
-                <svg
-  className="ts-arrow-svg"
-  aria-hidden="true"
-  viewBox="0 0 512 512"
-  xmlns="http://www.w3.org/2000/svg"
->
-  <path d="M504 256C504 119 393 8 256 8S8 119 8 256s111 248 248 248 248-111 248-248zm-448 0c0-110.5 89.5-200 200-200s200 89.5 200 200-89.5 200-200 200S56 366.5 56 256zm72 20v-40c0-6.6 5.4-12 12-12h116v-67c0-10.7 12.9-16 20.5-8.5l99 99c4.7 4.7 4.7 12.3 0 17l-99 99c-7.6 7.6-20.5 2.2-20.5-8.5v-67H140c-6.6 0-12-5.4-12-12z" />
-</svg>
-                Streamline processes and empower your organization in the digital era
-              </li>
-            </ul>
+
+            <p>
+              The rapid evolution of technology can be both an opportunity and a
+              challenge. Our solutions harness the latest advancements to
+              enhance operational efficiency, streamline processes and empower
+              your organization to stay ahead in the digital era.
+            </p>
           </div>
+
           <div className="ts-expertise-item">
             <h3>Custom-Tailored Strategies</h3>
-            <ul>
-              <li>
-                <svg
-  className="ts-arrow-svg"
-  aria-hidden="true"
-  viewBox="0 0 512 512"
-  xmlns="http://www.w3.org/2000/svg"
->
-  <path d="M504 256C504 119 393 8 256 8S8 119 8 256s111 248 248 248 248-111 248-248zm-448 0c0-110.5 89.5-200 200-200s200 89.5 200 200-89.5 200-200 200S56 366.5 56 256zm72 20v-40c0-6.6 5.4-12 12-12h116v-67c0-10.7 12.9-16 20.5-8.5l99 99c4.7 4.7 4.7 12.3 0 17l-99 99c-7.6 7.6-20.5 2.2-20.5-8.5v-67H140c-6.6 0-12-5.4-12-12z" />
-</svg>
-                Highly personalized approach aligned with your specific challenges
-              </li>
-              <li>
-                <svg
-  className="ts-arrow-svg"
-  aria-hidden="true"
-  viewBox="0 0 512 512"
-  xmlns="http://www.w3.org/2000/svg"
->
-  <path d="M504 256C504 119 393 8 256 8S8 119 8 256s111 248 248 248 248-111 248-248zm-448 0c0-110.5 89.5-200 200-200s200 89.5 200 200-89.5 200-200 200S56 366.5 56 256zm72 20v-40c0-6.6 5.4-12 12-12h116v-67c0-10.7 12.9-16 20.5-8.5l99 99c4.7 4.7 4.7 12.3 0 17l-99 99c-7.6 7.6-20.5 2.2-20.5-8.5v-67H140c-6.6 0-12-5.4-12-12z" />
-</svg>
-                Solutions that ensure maximum return on investment
-              </li>
-            </ul>
+
+            <p>
+              Recognizing that each organization is distinct, we take a highly
+              personalized approach. We collaborate closely with you to
+              understand your specific challenges and objectives. This enables
+              us to craft solutions that align perfectly with your requirements,
+              ensuring a maximum return on investment.
+            </p>
+          </div>
+
+          <div className="ts-expertise-item">
+            <h3>Holistic Technology Integration</h3>
+
+            <p>
+              Our services go beyond mere technology upgrades. We adopt a
+              holistic approach that considers your organization&apos;s culture,
+              employee engagement and leadership development, ensuring that
+              technology is seamlessly integrated and adopted throughout your
+              organization.
+            </p>
+          </div>
+
+          <div className="ts-expertise-item">
+            <h3>Incorporating Best Practices</h3>
+
+            <p>
+              Our technology consulting solutions are underlined by industry
+              best practices. We draw from tried-and-tested methodologies and
+              enhance them with innovative thinking, providing you with a
+              well-rounded, dependable and forward-looking approach.
+            </p>
+          </div>
+
+          <div className="ts-expertise-item">
+            <h3>Innovative Management Techniques</h3>
+
+            <p>
+              Our team of technology consultants brings creative and adaptive
+              management techniques to the table. We empower your leadership
+              with innovative strategies to effectively guide your organization
+              through technological transformation.
+            </p>
           </div>
         </div>
       </section>
-
       {/* EXPERTISE QUALITIES */}
-      <section className="ts-qualities">
+      {/* <section className="ts-qualities">
         <h2>Why Choose Taciti Technology Services</h2>
         <div className="ts-qualities-grid">
-
           <div className="ts-quality-card">
             <h3>Holistic Technology Integration</h3>
             <p>
-              Our services go beyond mere technology upgrades. We adopt a holistic approach
-              that considers your organization&apos;s culture, employee engagement and leadership
-              development, ensuring that technology is seamlessly integrated and adopted
-              throughout your organization.
+              Our services go beyond mere technology upgrades. We adopt a
+              holistic approach that considers your organization&apos;s culture,
+              employee engagement and leadership development, ensuring that
+              technology is seamlessly integrated and adopted throughout your
+              organization.
             </p>
           </div>
 
           <div className="ts-quality-card">
             <h3>Incorporating Best Practices</h3>
             <p>
-              Our technology consulting solutions are underlined by industry best practices.
-              We draw from tried-and-tested methodologies and enhance them with innovative
-              thinking, providing you with a well-rounded, dependable and forward-looking
-              approach.
+              Our technology consulting solutions are underlined by industry
+              best practices. We draw from tried-and-tested methodologies and
+              enhance them with innovative thinking, providing you with a
+              well-rounded, dependable and forward-looking approach.
             </p>
           </div>
 
           <div className="ts-quality-card">
             <h3>Innovative Management Techniques</h3>
             <p>
-              Our team of technology consultants brings creative and adaptive management
-              techniques to the table. We empower your leadership with innovative strategies
-              to effectively guide your organization through technological transformation.
+              Our team of technology consultants brings creative and adaptive
+              management techniques to the table. We empower your leadership
+              with innovative strategies to effectively guide your organization
+              through technological transformation.
             </p>
           </div>
-
         </div>
-      </section>
-
+      </section> */}
     </div>
   );
 }
